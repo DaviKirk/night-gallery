6370bd8f48e8de575a700e6553e9c1ee128444a3
DaviKirk <davikirk2008@gmail.com>
2025-10-31 02:58:23 -0300

    Adding responsiveness for mobile devices

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ if __name__ == '__main__':
     with app.test_request_context():
         db.create_all()
             
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
